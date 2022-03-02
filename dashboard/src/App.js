2c21fdd1b91654924bb1b5d38166a97c50df2e5c
Juan Carlos <juan.campbell@buyersedgeplatform.com>
2022-03-02 12:37:44 -0600

    Parse data

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,12 +1,9 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
 import Axios from 'axios';
 
-const dataProvider = jsonServerProvider('http://localhost:3001');
-
 const apiClient = Axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://localhost:8071',
   headers: {
     'Pragma': 'no-cache',
     'Content-Type': 'application/json; charset=UTF-8',
@@ -14,20 +11,21 @@ const apiClient = Axios.create({
 });
 
 apiClient.interceptors.response.use((response) => {
-  console.log(response);
   return {
     ...response,
-    total: 5
+    total: response.data.length,
   };
 }, (error) => {
   return Promise.reject(error);
 }, {synchronous: true});
 
 const dataProvider2 = {
-  getList:    (resource, params) => {
+  getList: (resource, params) => {
     return apiClient.get(`/${resource}`);
   },
-  getOne:     (resource, params) => { return new Promise((resolve, reject) => {resolve('Hi')});},
+  getOne: (resource, params) => {
+    return apiClient.get(`/${resource}`);
+  },
   getMany:    (resource, params) => { return new Promise((resolve, reject) => {resolve('Hi')});},
   getManyReference: (resource, params) => { return new Promise((resolve, reject) => {resolve('Hi')});},
   create:     (resource, params) => { return new Promise((resolve, reject) => {resolve('Hi')});},
