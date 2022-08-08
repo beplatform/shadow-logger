e8cb6fd70804ae6ea3cf067b5f41aa205128113d
Anuark <jaicof@gmail.com>
2022-08-08 17:48:08 -0600

    pino transport update

--- dist/main.d.ts
+++
@@ -1,4 +0,0 @@
-declare const serveDebugger: (debuggerUrlPath?: string, debuggerBuildPath?: string) => import("express-serve-static-core").Router;
-declare const serveFSLogger: (logsApiUrl?: string, logsPath?: string) => import("express-serve-static-core").Router;
-declare const serveS3Logger: (logsApiUrl: string, bucket?: string, path?: string) => import("express-serve-static-core").Router;
-export { serveDebugger, serveFSLogger, serveS3Logger };
