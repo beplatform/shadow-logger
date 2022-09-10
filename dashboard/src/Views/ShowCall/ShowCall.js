c1f0474a216527d4fb5b5e15900616be98788195
Anuark <jaicof@gmail.com>
2022-09-09 19:26:36 -0600

    ShowCall view update

--- dashboard/src/Views/ShowCall/ShowCall.js
+++ dashboard/src/Views/ShowCall/ShowCall.js
@@ -4,35 +4,35 @@ import { JsonField } from 'react-admin-json-view';
 import ArrayLogs from './ArrayLogs';
 
 const ShowCall = (props) => {
-	return (
-		<TabbedShowLayout {...props}>
-			<Tab label='request'>
-				<TextField source='id' label='Request ID' />
-				<hr/>
-        <TextField source='request.method' label='Method' />
-        <DateField source='request.time' label='Request Time' showTime />
-        <TextField source='request.url' label='URL' />
-        <TextField source='request.ip' label='IP' />
-        <JsonField source='request.headers' label='Headers' addLabel={true} reactJsonOptions={{name: null, collapsed: false, displayDataTypes: false}} />
-        <JsonField source='request.body' label='Body' addLabel={true} reactJsonOptions={{name: null, collapsed: false, displayDataTypes: false}} />
-			</Tab>
-			<Tab label='response'>
-				<TextField source='id' label='Request ID' />
-				<hr/>
-        <TextField source='response.status' label='Status' color='red' />
-        <DateField source='response.time' label='Response Time' showTime />
-        {
-          props.record && props.record.response && props.record.response.body && typeof props.record.response.body === 'object' ?
-          <JsonField source='response.body' label='Body' addLabel={true} reactJsonOptions={{name: null, collapsed: false, displayDataTypes: false}} />
-          :
-          <TextField source='response.body' label='Body' />
-        }
-			</Tab>
-			<Tab label='logs'>
-        <ArrayLogs />
-			</Tab>
-		</TabbedShowLayout>
-	);
+    return (
+        <TabbedShowLayout {...props}>
+            <Tab label='request'>
+                <TextField source='id' label='Request ID' />
+                <hr />
+                <TextField source='method' label='Method' />
+                <DateField source='time' label='Request Time' showTime />
+                <TextField source='url' label='URL' />
+                <TextField source='ip' label='IP' />
+                <JsonField source='headers' label='Headers' addLabel={true} reactJsonOptions={{ name: null, collapsed: false, displayDataTypes: false }} />
+                <JsonField source='body' label='Body' addLabel={true} reactJsonOptions={{ name: null, collapsed: false, displayDataTypes: false }} />
+            </Tab>
+            <Tab label='response'>
+                <TextField source='id' label='Request ID' />
+                <hr />
+                <TextField source='statusCode' label='Status' color='red' />
+                <DateField source='time' label='Response Time' showTime />
+                {
+                    props.record && props.record.response && props.record.response.body && typeof props.record.response.body === 'object' ?
+                        <JsonField source='body' label='Body' addLabel={true} reactJsonOptions={{ name: null, collapsed: false, displayDataTypes: false }} />
+                        :
+                        <TextField source='body' label='Body' />
+                }
+            </Tab>
+            <Tab label='logs'>
+                <ArrayLogs />
+            </Tab>
+        </TabbedShowLayout>
+    );
 };
 
 export default ShowCall;
